c7220fe2feb25bf92bc7b03270e29d29a48d8d97
Yann Figueiredo <yann.fabricio@hotmail.com>
2021-11-19 14:27:42 -0300

    Fianlizada versão mobile e JavaScript do menu

---
+++ js/main.js
@@ -0,0 +1,6 @@
+(function(){
+    var menu_header = new Menu({
+        menu: '.menu',
+        btn_menu : '.btn-menu'
+    });
+}());
---
+++ js/menu.js
@@ -0,0 +1,32 @@
+function Menu(config){
+    this.menu = document.querySelector(config.menu);
+    this.btn_menu = document.querySelector(config.btn_menu);
+
+    _this = this;
+
+    _this.btn_menu.addEventListener('click', function(){
+        if(_this.btn_menu.classList.contains('btn-menu-aberto')){
+            fecharMenu();
+        }else{
+            abrirMenu();
+        }
+    });
+
+    function abrirMenu(){
+        _this.btn_menu.classList.add('btn-menu-aberto');
+        _this.btn_menu.classList.remove('fa-bars');
+        _this.btn_menu.classList.add('fa-times');
+        _this.menu.classList.remove('menu-escondido');
+
+        _this.menu.style.transform = 'translateX(0px)';
+    }
+
+    function fecharMenu(){
+        _this.btn_menu.classList.remove('btn-menu-aberto');
+        _this.btn_menu.classList.add('fa-bars');
+        _this.btn_menu.classList.remove('fa-times');
+        _this.menu.classList.add('menu-escondido');
+
+        _this.menu.style.transform = 'translateX(200px)';
+    }
+}
